34c292c127050aab5f6f03899cc362066a539148
Sai Pavan <saipavan@nimmetry.com>
2019-04-02 16:54:27 +0530

    enabled lastName field
    modifed files are: student.py && app.py

--- student.py
+++ student.py
@@ -4,9 +4,10 @@ class Student :
 
     school_name = "Sprinfield Elemnetary "
 
-    def __init__(self,name,student_id=332):
+    def __init__(self,name,last_name,student_id=332,):
         self.name = name
         self.student_id = student_id
+        self.last_name = last_name
         students.append(self)
 
     def __str__(self):
@@ -14,6 +15,10 @@ class Student :
 
     def get_name_capitalize(self):
         return  self.name.capitalize()
+
+    def get_last_name_capitalize(self):
+        return  self.last_name.capitalize()
+
 
     def get_school_name(self):
         return self.school_name
