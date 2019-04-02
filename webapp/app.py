576a57c5eb58e6e3411d15b7c6b31320e4ceeb98
Sai Pavan <saipavan@nimmetry.com>
2019-04-02 17:56:10 +0530

    Student to Employee
    Changed app base on Student to Employee.  Added Designation Field to the code.

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template,redirect, url_for, request
 
-from student import Student
+from employee import Employee
 
-students = []
+employees = []
 
 app = Flask(__name__)
 
@@ -10,16 +10,19 @@ app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def students_page():
     if request.method == "POST":
-        new_student_id = request.form.get("student-id","")
-        new_student_name = request.form.get("name","")
-        new_student_last_name = request.form.get("last-name","")
+        new_employee_id = request.form.get("employee-id","")
+        new_employee_name = request.form.get("name","")
+        new_employee_last_name = request.form.get("last-name","")
+        new_employee_designation = request.form.get("designation","")
 
-        new_student = Student(name=new_student_name, student_id=new_student_id)
-        students.append(new_student)
+
+        new_employee = Employee(name=new_employee_name,last_name=new_employee_last_name,designation=new_employee_designation, employee_id=new_employee_id)
+        print(new_employee)
+        employees.append(new_employee)
 
         return redirect(url_for("students_page"))
 
-    return render_template("index.html", students=students)
+    return render_template("index.html", employees=employees)
 
 
 if __name__ == "__main__":
